c2938b42397f033be8e01fd83406dc72db7e5ca7
putrilauraa <rahmpautrilaura@gmail.com>
2025-06-14 04:33:47 +0700

    back to home button

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ function NotFound() {
       <h2 className="text-2xl font-bold text-red-600">404 - Page Not Found</h2>
       <p className="mt-2">Oops! We couldn't find that page.</p>
       <Link to="/">
-        <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500">
+        <button className="mt-4 px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-400">
           Back to Home
         </button>
       </Link>
